ea6d7a79af79a222d0acca61fc49f524ed574203
Vamsi <vamsitupakula474@gmail.com>
2023-09-22 20:26:51 +0530

    Edit button added for todo items...

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Todo(props) {
+  const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState(props.item.task);
   const markCompleted = () => {
     const lists = [...props.tasks];
     lists[props.index].status = "done";
@@ -12,12 +14,39 @@ export default function Todo(props) {
     props.setTasks(my_tasks.filter((item, index) => index !== props.index));
   };
 
+  const handleEdit = () => {
+    if (editing) {
+      const my_tasks = [...props.tasks];
+      my_tasks.map((task, index) => {
+        if (index === props.index) {
+          task.task = value;
+        }
+        return task;
+      });
+
+      props.setTasks(my_tasks);
+    }
+    setEditing((prev) => !prev);
+  };
+
   return (
     <li
       className={`flex justify-between align-middle my-1 py-2 px-3 border-2 rounded-md shadow-sm shadow-gray-300 hover:shadow-gray-400`}
     >
       <div className="flex flex-col gap-y-1">
-        <p className="text-lg font-semibold">{props.item.task}</p>
+        {editing ? (
+          <input
+            type="text"
+            value={value}
+            className="border border-green-600 outline-none py-1 px-2 text-lg rounded-sm font-semibold"
+            onChange={(e) => setValue(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.keyCode === 13) handleEdit();
+            }}
+          />
+        ) : (
+          <p className="text-lg font-semibold">{props.item.task}</p>
+        )}
         <div className="flex align-start flex-wrap w-100 gap-x-2">
           {props.item.tags !== undefined &&
             props.item.tags.slice(0, 3).map((tag, index) => {
@@ -34,6 +63,13 @@ export default function Todo(props) {
         </div>
       </div>
       <div className="btn-group flex gap-x-4 text-xl pr-2">
+        <button type="button" className={`btn`} onClick={handleEdit}>
+          {editing ? (
+            <i class="fa-solid fa-floppy-disk text-green-600 hover:text-green-700"></i>
+          ) : (
+            <i className="fa-solid fa-pen-to-square text-green-600 hover:text-green-700"></i>
+          )}
+        </button>
         {props.done !== true ? (
           <button type="button" className={`btn`} onClick={deleteTask}>
             <i className="fa-solid fa-trash text-red-600 hover:text-red-700"></i>
